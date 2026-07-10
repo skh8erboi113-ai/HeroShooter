a29d4a212bd53d7b4f466b5b4a0f52f6b38bf5b2
Brenden <sk.h8er.boi113@gmail.com>
2026-07-10 03:54:27 -0700

    Add settings.gradle.kts for project configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+// settings.gradle.kts
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // AGDK Maven repository for game libraries
+        maven { url = uri("https://dl.google.com/android/maven2") }
+    }
+}
+
+rootProject.name = "HeroShooterEngine"
+include(":app")
